cdbf205448338b1d02d16faf09ccb4959c123fdb
Euro-pol <52547763+Euro-pol@users.noreply.github.com>
2023-09-03 16:31:28 +0200

    finished code (working)

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-import flask
+import playwright.sync_api
 import requests
-import os
+import flask
 import json
+import time
+import os
 
-app = flask.Flask(__name__)
-
-def download_solver(): #please body dont sue me
+def setup_solver(): #please body dont sue me
     if not os.path.exists("utils"): os.mkdir("utils")
     files = ["https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/solver.py", "https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/page.html"]
     for file in files:
         r = requests.get(file).text
         with open("utils/" + file.split("/")[-1], "w") as f:
-            f.write(r)        
+            f.write(r)
+
+setup_solver()
+app = flask.Flask(__name__)
+from utils import solver
+
+@app.route("/solve", methods=["POST"])
+def solve():
+    json_data = flask.request.json
+    sitekey = json_data["sitekey"]
+    invisible = json_data["invisible"]
+    url = json_data["url"]
+    with playwright.sync_api.sync_playwright() as p:
+        s = solver.Solver(p, headless=True)
+        start_time = time.time()
+        token = s.solve(url, sitekey, invisible)
+        #print(f"took {time.time() - start_time} seconds")
+        return make_response(token)
+
+def make_response(captcha_key):
+    if captcha_key == "failed":
+        return flask.jsonify({"status": "error", "message": "Captcha was not solved"})
+    return flask.jsonify({"status": "success", "token": captcha_key})
+
+if __name__ == "__main__":
+    app.run()
